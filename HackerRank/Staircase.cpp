018bcae052f6c1195903d71923c9235f3dc87110
Raihan-Chowdhury <RaihanChowdhury.next@gmail.com>
2021-02-02 11:09:14 +0600

    Pulling

--- HackerRank/Staircase.cpp
+++ HackerRank/Staircase.cpp
@@ -1,12 +1,55 @@
 /** Problem link: https://www.hackerrank.com/challenges/staircase/problem **/
 
-#include<cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+void ove();
+void showvike();
+void raj();
 void raihan();
+void sazol();
+
 int main()
 {
-	raihan();
-	return 0;
+  ove();
+	raj();
+  sazol();
+  raihan();
+	showvike();
+  return 0;
 }
+
+
+
+void ove()
+{
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n-i; j++)
+            printf(" ");
+        for(j=1; j<=i; j++)
+            printf("#");
+        printf("\n");
+    }
+}
+
+void sazol() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n - 1 - i; ++j) {
+            printf(" ");
+        }
+        for(int j = 0; j <= i; ++j) {
+            printf("#");
+        }
+        puts("");
+    }
+}
+
 void raihan(){
 	int n;
 	scanf("%d",&n);
@@ -17,4 +60,32 @@ void raihan(){
 		}
 		printf("\n");
 	}
+}
+
+void raj(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=n-1;j>=0;j--){
+			if(i>=j)cout<<"#";
+			else cout<<" ";
+		}
+		cout<<endl;
+  }
+}
+
+void showvike() {
+	int n;
+
+	while(scanf("%d", &n) == 1)
+		for(int I = 0; I < n; I++)
+		{
+			for(int K = I; K < n - 1; K++)
+				printf(" ");
+
+			for(int K = 0; K <= I; K++)
+				printf("#");{
+
+			printf("\n");
+		}
 }
--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -1,11 +1,33 @@
 /** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
 
-#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+void ove();
+void showvike();
+void raj();
+void jayem();
 void raihan();
+
 int main()
 {
+    ove();
+	  showvike();
+	  raj();
+    jayem();
     raihan();
     return 0;
+}
+
+void ove()
+{
+    int t, a, b, i;
+    scanf("%d", &t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d%d", &a, &b);
+        printf("Case %lld: %lld\n", i, a+b);
+    }
 }
 void raihan(){
     int n;
@@ -15,4 +37,49 @@ void raihan(){
         scanf("%d%d",&t1,&t2);
         printf("Case %d: %d\n",x+1,t1+t2);
     }
+}
+
+void jayem()
+{
+    int tcase,a,b,i;
+
+    scanf("%d",&tcase);
+
+    for(i=1;i<=tcase;i++)
+    {
+        scanf("%d%d",&a,&b);
+        printf("Case %d: %d\n",i,a+b);
+    }
+}
+
+void raj(){
+	int t;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+    int a,b;
+    cin>>a>>b;
+    cout<<"Case "<<i<<": "<<a+b<<endl;
+	}
+}
+
+void showvike() {
+	int t;
+
+	while(scanf("%d", &t) == 1)
+	{
+		int I = 0;
+
+		while(t--)
+		{
+			int a, b;
+
+			scanf("%d %d", &a, &b);
+
+			int c = a + b;
+
+			printf("Case %d: %d\n", I + 1, c);
+
+			I++;
+		}
+	}
 }
